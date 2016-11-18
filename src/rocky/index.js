888ad2e0656e06ad8d39789a3170a4fccca0926f
lesyk <lesyk@users.noreply.github.com>
2016-11-18 17:13:06 +0100

    Add mars time func

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,33 +1,36 @@
 var rocky = require('rocky');
 
+function h_to_hms(h) {
+  var x = h * 3600;
+  var hh = Math.floor(x / 3600);
+  if (hh < 10) hh = "0" + hh;
+  var y = x % 3600;
+  var mm = Math.floor(y / 60);
+  if (mm < 10) mm = "0" + mm;
+  var ss = Math.round(y % 60);
+  if (ss < 10) ss = "0" + ss;
+  return hh + ":" + mm + ":" + ss;
+}
+
 rocky.on('draw', function(event) {
-  // Get the CanvasRenderingContext2D object
+  var d = new Date();
+  var millis = d.getTime();
+  var jd_ut = 2440587.5 + (millis / 8.64E7);
+  var jd_tt = jd_ut + (35 + 32.184) / 86400;
+  var j2000 = jd_tt - 2451545.0;
+  var msd = (((j2000 - 4.5) / 1.027491252) + 44796.0 - 0.00096);
+  var mtc = (24 * msd) % 24;
+
   var ctx = event.context;
-
-  // Clear the screen
   ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-
-  // Determine the width and height of the display
   var w = ctx.canvas.unobstructedWidth;
   var h = ctx.canvas.unobstructedHeight;
-
-  // Current date/time
-  var d = new Date();
-
-  // Set the text color
   ctx.fillStyle = 'white';
-
-  // Center align the text
   ctx.textAlign = 'center';
-
-  // Display the time, in the middle of the screen
-  ctx.fillText(d.toLocaleTimeString(), w / 2, h / 2, w);
+  ctx.font = '42px light Bitham';
+  ctx.fillText(h_to_hms(mtc)+" SOL", w/2, h/4, w);
 });
 
 rocky.on('minutechange', function(event) {
-  // Display a message in the system logs
-  console.log("Another minute with your Pebble!");
-
-  // Request the screen to be redrawn on next pass
   rocky.requestDraw();
 });
